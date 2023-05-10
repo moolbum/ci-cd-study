d8fc41f772d4022d0260feae1c99f2b2a214fbd5
slogup-dale <dyddn304@gmail.com>
2023-05-10 11:11:04 +0900

    add: text

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ function App() {
       <p>{process.env.PORT}</p>
       <p>{process.env.NODE_ENV}</p>
       <p>{process.env.REACT_APP_ENV_MODE}</p>
-      <p>현재 배포</p>
+      <p>
+        현재 배포{" "}
+        {process.env.REACT_APP_ENV_MODE === "dev" ? "개발배포" : "운영배포"}
+      </p>
     </div>
   );
 }
